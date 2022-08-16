e51eab32491c42ac2bc126ea04118489dd4445c1
Precious <Preshomondi@gmail.com>
2022-08-16 05:18:46 +0300

    commit tree 482cc2b27c633172f4b4fb932704d9bb6f5beb50

---
+++ app.rb
@@ -0,0 +1,7 @@
+# # app.rb
+# p [0, 12]
+# print "Hello world!"
+# pp [{id: 1, hello: "World", me: "new"}, {id:2, hello:"Ruby"}, {id:3, hello: "Moon"}, {id:4, hello: "Learner"}]
+puts "Hello World!"
+print "Pass this test, please."
+p [1, 2, 3]
